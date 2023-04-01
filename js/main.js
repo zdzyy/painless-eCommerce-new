41da7dfdc6ece6cd3c56340e56ca6a61f9df0ae5
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-04-01 17:16:29 -0600

    base

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const productos = [
+    //hoodies
+    {
+        id: "nntnd",
+        titulo: "nintendo hoodie",
+        imagen: "./img/hoodies/nintendohoodieBack.png",
+        categoria: {
+            nombre: "Hoodies",
+            id: "hoodies"
+        },
+        precio: 1899.99
+    },
+    {
+        id: "pnlss",
+        titulo: "painless hoodie",
+        imagen: "./img/hoodies/painlesshodieeBack.png",
+        categoria: {
+            nombre: "Hoodies",
+            id: "hoodies"
+        },
+        precio: 1299.99
+    },
+    {
+        id: "rndm",
+        titulo: "rndm xclsv",
+        imagen: "./img/hoodies/rndmhoodie.png",
+        categoria: {
+            nombre: "Hoodies",
+            id: "hoodies"
+        },
+        precio: 799.99
+    },
+]
