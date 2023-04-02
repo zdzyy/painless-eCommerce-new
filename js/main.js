8877f32c4be636cffb4998f7534f56cd818e77da
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-04-02 14:07:34 -0600

    all css and some js

--- js/main.js
+++ js/main.js
@@ -31,3 +31,27 @@ const productos = [
         precio: 799.99
     },
 ]
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+
+function cargarProductos() {
+
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+}
+
+cargarProductos();
